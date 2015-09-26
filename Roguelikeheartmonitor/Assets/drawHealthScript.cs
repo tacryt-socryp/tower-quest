ae8feb39f60681a4c391615a7e015e1d4ce7469f
Bombergames <johnwblum@gmail.com>
2015-09-26 19:13:14 -0400

    stoof
    lots of stuff coming ou tin this patch folks, hold on to your testicles

---
+++ Roguelikeheartmonitor/Assets/drawHealthScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class drawHealthScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.DrawLine(Vector3.zero, new Vector3(100, 100, 000), Color.green);
+	}
+}
--- Roguelikeheartmonitor/Assets/dummyEnemyScript.cs
+++ Roguelikeheartmonitor/Assets/dummyEnemyScript.cs
@@ -4,9 +4,10 @@ using System.Collections;
 public class dummyEnemyScript : MonoBehaviour {
 
 	// Use this for initialization
+	Color startingColor;
 	public int health = 10;
 	void Start () {
-	
+		startingColor = this.GetComponent<SpriteRenderer>().color;
 	}
 	
 	// Update is called once per frame
@@ -19,12 +20,18 @@ public class dummyEnemyScript : MonoBehaviour {
 			var boxScript = coll.gameObject.GetComponent<hitBoxScript>();
 			if (boxScript) {
 				this.health -= boxScript.damage;
+				this.GetComponent<SpriteRenderer>().color = new Color(0.7f, 0.2f, 0.2f);
+				Invoke("MoveBack", 0.2f);
 				if (this.health <= 0) {
 					Die();
 				}
 			}
 			this.GetComponent<Rigidbody2D>().MovePosition(new Vector2(this.transform.position.x, this.transform.position.y) + new Vector2(this.transform.position.x - FindObjectOfType<PlayerScript>().transform.position.x, this.transform.position.y - FindObjectOfType<PlayerScript>().transform.position.y).normalized * 0.1f);
 		}
+	}
+
+	void MoveBack() {
+		this.GetComponent<SpriteRenderer>().color = startingColor;
 	}
 
 	void Die() {
