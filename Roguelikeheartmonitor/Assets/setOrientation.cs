d7a294f4e3c7f2da5c871dd30cd97f5154903cb8
Bombergames <johnwblum@gmail.com>
2015-09-26 19:39:19 -0400

    orientation setter
    sets the orientation, should be one in every scene

---
+++ Roguelikeheartmonitor/Assets/setOrientation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class setOrientation : MonoBehaviour {
+
+	// Use this for initialization
+	public ScreenOrientation o;
+	void Start () {
+		Screen.orientation = o;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
