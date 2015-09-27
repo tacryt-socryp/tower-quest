6314a85cc24d63d42fbf895513ec2734e664ce00
Bombergames <johnwblum@gmail.com>
2015-09-27 09:27:54 -0400

    player facing
    proper facing

--- Roguelikeheartmonitor/Assets/drawHealthScript.cs
+++ Roguelikeheartmonitor/Assets/drawHealthScript.cs
@@ -4,13 +4,16 @@ using System.Collections;
 public class drawHealthScript : MonoBehaviour {
 
 	GUIText text;
-	public GameObject pman;
+	public PlayerScript pman;
 	void Start () {
 		text = this.GetComponent<GUIText>() as GUIText;	
+		if (pman == null) {
+			pman = FindObjectOfType<PlayerScript>() as PlayerScript;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		text.text = pman.GetComponent<PlayerScript>().GetHealthMessage();
+		text.text = pman.GetHealthMessage();
 	}
 }
