b82ff002588f90dd27554f56fcaf1145061af780
Bombergames <johnwblum@gmail.com>
2015-09-26 23:42:02 -0400

    down cellar door man
    use to traverse levels

---
+++ Roguelikeheartmonitor/Assets/nextLevel.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class nextLevel : MonoBehaviour {
+
+	// Use this for initialization
+	public string nextScene;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.name == "Player") {
+			Application.LoadLevel(nextScene);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		if (coll.name == "Player") {
+			Application.LoadLevel(nextScene);
+		}
+	}
+}
