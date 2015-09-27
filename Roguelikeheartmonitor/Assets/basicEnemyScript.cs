e87698c123fc0e41aa5b350e86ce0ad9db607897
Logan Allen <loganallc@gmail.com>
2015-09-27 00:32:30 -0400

    Enemies collide and hurt you.

--- Roguelikeheartmonitor/Assets/basicEnemyScript.cs
+++ Roguelikeheartmonitor/Assets/basicEnemyScript.cs
@@ -19,17 +19,38 @@ public class basicEnemyScript : MonoBehaviour {
 	
 	void OnCollisionEnter2D(Collision2D coll) {
 		if (coll.gameObject.tag == "hitBox") {
-			var boxScript = coll.gameObject.GetComponent<hitBoxScript>();
+			var boxScript = coll.gameObject.GetComponent<hitBoxScript> ();
 			if (boxScript) {
 				this.health -= boxScript.damage;
 				if (this.health <= 0) {
-					Die();
+					Die ();
 				}
-				this.GetComponent<SpriteRenderer>().color = new Color(0.7f, 0.2f, 0.2f);
-				Invoke("MoveBack", 0.2f);
+				this.GetComponent<SpriteRenderer> ().color = new Color (0.7f, 0.2f, 0.2f);
+				Invoke ("MoveBack", 0.2f);
 			}
-			this.GetComponent<Rigidbody2D>().MovePosition(new Vector2(this.transform.position.x, this.transform.position.y) + new Vector2(this.transform.position.x - FindObjectOfType<PlayerScript>().transform.position.x, this.transform.position.y - FindObjectOfType<PlayerScript>().transform.position.y).normalized * 0.1f);
+			this.GetComponent<Rigidbody2D> ().MovePosition (new Vector2 (this.transform.position.x, this.transform.position.y) + new Vector2 (this.transform.position.x - FindObjectOfType<PlayerScript> ().transform.position.x, this.transform.position.y - FindObjectOfType<PlayerScript> ().transform.position.y).normalized * 0.1f);
+		} else {
+			var playerScript = coll.gameObject.GetComponent<PlayerScript> ();
+			if (playerScript) {
+				Transform t = playerScript.GetComponent<Transform> ();
+
+				playerScript.GetComponent<Rigidbody2D> ().AddForce(new Vector2(
+					(t.position.x - this.transform.position.x),
+					(t.position.y - this.transform.position.y)
+				).normalized * 150f);
+				playerScript.enabled = false;
+				playerScript.currentHealth = playerScript.currentHealth - 1;
+				playerScript.GetComponent<SpriteRenderer>().color = new Color(0.7f, 0.2f, 0.2f);
+				playerScript.Invoke("MoveBack", 0.2f);
+				Invoke ("enablePlayer", 0.25f);
+			}
+
+			this.GetComponent<Rigidbody2D> ().MovePosition (new Vector2 (this.transform.position.x, this.transform.position.y) - new Vector2 (this.transform.position.x - FindObjectOfType<PlayerScript> ().transform.position.x, this.transform.position.y - FindObjectOfType<PlayerScript> ().transform.position.y).normalized * 0.1f);
 		}
+	}
+
+	void enablePlayer() {
+		FindObjectOfType<PlayerScript> ().enabled = true;
 	}
 
 	void Attack() {
